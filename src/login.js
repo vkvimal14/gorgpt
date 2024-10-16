0643d1226204b5dffbc05703b8debd4cb53d916e
Vimal Kumar <vkvimal14@gmail.com>
2024-10-16 23:29:46 +0530

    backend url update

--- src/login.js
+++ src/login.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function Login({ setUser }) {
   const handleLogin = async () => {
     try {
-      const response = await axios.get('https://your-backend-url/login');
+      const response = await axios.get('https://100.20.92.101/login');
       const { user } = response.data;
       localStorage.setItem('user', JSON.stringify(user));
       setUser(user);
